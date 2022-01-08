f736e98c8da7addcec8e1e4fc2f6022d365dda27
Pawel Harasimiuk <p.harasimiuk@gmail.com>
2022-01-08 15:37:25 -0800

    copied hello-kubernetes and updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,32 @@
-# This is a multi-stage build. First we are going to compile and then
-# create a small image for runtime.
-FROM golang:1.11.1 as builder
+FROM node:16-alpine3.13
 
-RUN mkdir -p /go/src/github.com/eks-workshop-sample-api-service-go
-WORKDIR /go/src/github.com/eks-workshop-sample-api-service-go
-RUN useradd -u 10001 app
-COPY . .
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
+ARG IMAGE_CREATE_DATE
+ARG IMAGE_VERSION
+ARG IMAGE_SOURCE_REVISION
 
-FROM scratch
+# Metadata as defined in OCI image spec annotations - https://github.com/opencontainers/image-spec/blob/master/annotations.md
+LABEL org.opencontainers.image.title="Hello Kubernetes!" \
+      org.opencontainers.image.description="Provides a demo app to deploy to a Kubernetes cluster. It displays a message, the name of the pod and details of the node it is deployed to." \
+      org.opencontainers.image.created=$IMAGE_CREATE_DATE \
+      org.opencontainers.image.version=$IMAGE_VERSION \
+      org.opencontainers.image.authors="Paul Bouwer" \
+      org.opencontainers.image.url="https://hub.docker.com/r/paulbouwer/hello-kubernetes/" \
+      org.opencontainers.image.documentation="https://github.com/paulbouwer/hello-kubernetes/README.md" \
+      org.opencontainers.image.vendor="Paul Bouwer" \
+      org.opencontainers.image.licenses="MIT" \
+      org.opencontainers.image.source="https://github.com/paulbouwer/hello-kubernetes.git" \
+      org.opencontainers.image.revision=$IMAGE_SOURCE_REVISION 
 
-COPY --from=builder /go/src/github.com/eks-workshop-sample-api-service-go/main /main
-COPY --from=builder /etc/passwd /etc/passwd
-USER app
+# Create app directory
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
 
-EXPOSE 8080
-CMD ["/main"]
+# Install app dependencies
+COPY package.json /usr/src/app/
+RUN npm install
+
+# Bundle app source
+COPY . /usr/src/app
+
+USER node
+CMD [ "npm", "start" ]
---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const os = require("os");
+const fs = require('fs');
+
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+const expressLogger = expressPino({ logger });
+
+const app = express();
+app.use(expressLogger);
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Configuration
+
+var port = process.env.PORT || 8080;
+var message = process.env.MESSAGE || "I don't know how to set up an env variable apparently";
+var renderPathPrefix = (
+  process.env.RENDER_PATH_PREFIX ? 
+    '/' + process.env.RENDER_PATH_PREFIX.replace(/^[\\/]+/, '').replace(/[\\/]+$/, '') :
+    ''
+);
+var handlerPathPrefix = (
+  process.env.HANDLER_PATH_PREFIX ? 
+    '/' + process.env.HANDLER_PATH_PREFIX.replace(/^[\\/]+/, '').replace(/[\\/]+$/, '') :
+    ''
+);
+
+var namespace = process.env.MY_POD_NAMESPACE || 'default';
+var podName = process.env.KUBERNETES_POD_NAME || os.hostname();
+var nodeName = process.env.KUBERNETES_NODE_NAME || '-';
+var nodeOS = os.type() + ' ' + os.release();
+var podIP = process.env.MY_POD_IP
+var podHost = process.env.POD_HOST
+var applicationVersion = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
+var containerImage = process.env.CONTAINER_IMAGE || 'paulbouwer/hello-kubernetes:' + applicationVersion
+var containerImageArch = JSON.parse(fs.readFileSync('info.json', 'utf8')).containerImageArch;
+
+logger.debug();
+logger.debug('Configuration');
+logger.debug('-----------------------------------------------------');
+logger.debug('PORT=' + process.env.PORT);
+logger.debug('MESSAGE=' + process.env.MESSAGE);
+logger.debug('RENDER_PATH_PREFIX=' + process.env.RENDER_PATH_PREFIX);
+logger.debug('HANDLER_PATH_PREFIX=' + process.env.HANDLER_PATH_PREFIX);
+logger.debug('KUBERNETES_NAMESPACE=' + process.env.KUBERNETES_NAMESPACE);
+logger.debug('KUBERNETES_POD_NAME=' + process.env.KUBERNETES_POD_NAME);
+logger.debug('KUBERNETES_NODE_NAME=' + process.env.KUBERNETES_NODE_NAME);
+logger.debug('CONTAINER_IMAGE=' + process.env.CONTAINER_IMAGE);
+
+// Handlers
+
+logger.debug();
+logger.debug('Handlers');
+logger.debug('-----------------------------------------------------');
+
+logger.debug('Handler: static assets');
+logger.debug('Serving from base path "' + handlerPathPrefix + '"');
+app.use(handlerPathPrefix, express.static('static'))
+
+logger.debug('Handler: /');
+logger.debug('Serving from base path "' + handlerPathPrefix + '"');
+app.get(handlerPathPrefix + '/', function (req, res) {
+    res.render('home', {
+      message: message,
+      namespace: namespace,
+      pod: podName,
+      node: nodeName + ' (' + nodeOS + ')',
+      container: containerImage + ' (' + containerImageArch + ')',
+      renderPathPrefix: renderPathPrefix,
+      podhost: podHost,
+      podip: podIP,
+    });
+});
+
+// Server
+
+logger.debug();
+logger.debug('Server');
+logger.debug('-----------------------------------------------------');
+
+app.listen(port, function () {
+  logger.info("Listening on: http://%s:%s", podName, port);
+});
